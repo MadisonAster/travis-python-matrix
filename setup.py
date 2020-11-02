d8700e9c3dd1ed2ab1e0a4f4e146b6f7c8c5361b
Madison Aster <info@MadisonAster.com>
2020-11-01 20:41:49 -0800

    2. 7

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
             'Intended Audience :: Developers',
             'Topic :: Software Development',
             'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+            'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
